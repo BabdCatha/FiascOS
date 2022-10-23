31860b1d011b7fd7a7cef5a1deabcf151b0a2ae3
BabdCatha <36963178+BabdCatha@users.noreply.github.com>
2022-10-23 22:39:12 +0200

    Add disk reading capabilities

--- Code/main.asm
+++ Code/main.asm
@@ -5,6 +5,19 @@
 	mov bx, BOOT_MSG_16BIT 	;bx should contain the string address wehn calling print
 	call print 		;printing a message showing that we booted in 16bit mode
 
+	;; loading code from the second sector of the disk
+	;; dl is set to the boot disk by the BIOS when booting, and hasn't been modified yet
+	mov ah, 0x02 		;setting a read operation
+	mov dh, 0x01 		;reading 512 bytes (1 sector)
+	mov cl, 0x02 		;the first sector after the boot sector
+	mov ch, 0x00 		;cylinder 0
+
+	;setting the address (es is 0x0000)
+	mov bx, 0x9000
+	;; [es:bx] = 0x9000
+
+	call disk_load_16bits
+	
 	call switch_to_32bit
 	jmp $ 			;this is a failsafe in case of an error, this should never be executed
 
@@ -55,8 +68,51 @@ start_32bit:			;start of the 32bit mode instructions
 	call vga_print 		;we call the print function
 	
 	hlt 			;we stop the CPU
+
+	;; ----------------------16 bit disk read function------------------------- ;;
+
+	;; [es;bx] should contain the memory address to which the data will be written
+	
+	;; ah should contain the operation code (0x02 for read)
+	;; dl should contain the disk (0x00 for floppy1, 0x80 for hdd1...)
+	;; dh should contain the number of sectors to read (between 0x01 and 0x80)
+	;; cl should contain the sector to read (between 0x01 and 0x11)
+	;; ch should contain the cylinder (between 0x0 and 0x3ff)
+
+[bits 16]
+disk_load_16bits:
+	pusha
+	push dx 		;the dx register will need to be overwritten, and is
+	;; necessary to keep to later know if we read the correct amount of sectors
+
+	mov al, dh 		;setting the numbers of sectors to read
+	mov dh, 0 		;head 0
+	
+	int 0x13 		;BIOS interrupt for disk operation
+	jc disk_read_error 	;if an error occuped (stored in the carry bit)
+
+	;; if no error occured
+	pop dx 			;we restore dx to compare to what was read
+	cmp al, dh 		;doing the comparison
+	jne disk_sectors_error 	;in case of an error
+
+	;; no error at all
+	popa 			;we restore the registers
+	ret			;we return
+
+disk_read_error:
+	mov bx, DISK_READ_ERROR
+	call print 		;printing the error in 16bit mode
+	jmp $
+
+disk_sectors_error:
+	mov bx, DISK_SECTORS_ERROR
+	call print
+	jmp $
 	
 
+	;; ----------------------16 bit disk read function------------------------- ;;
+	
 	;; ---------------------16 bit mode print function------------------------- ;;
 [bits 16]
 print:
@@ -180,7 +236,7 @@ vga_print_last_line_clear_loop:
 	
 	;; ---------------------32 bit mode print function------------------------- ;;
 
-	;; ---------------------32 bit mode scroll function------------------------ ;;
+	;; ----------------------32 bit mode clear screen-------------------------- ;;
 
 [bits 32]
 vga_clear_screen:
@@ -247,5 +303,10 @@ gdt_descriptor:
 	BOOT_MSG_16BIT db "starting in 16bit mode", 0
 	BOOT_MSG_32BIT db "initialized 32bit mode", 0
 
+	DISK_READ_ERROR db "Error while reading disk", 0
+	DISK_SECTORS_ERROR db "Wrong number of sectors read", 0
+
 times 510-($-$$) db 0 		;filling the first sector up to the magic bytes
 dw 0xaa55 			;the magic bytes to inform the BIOS this disk is bootable
+
+times 1024 dw 0xbeef 		;dummy data for now
